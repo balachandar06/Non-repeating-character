803e07eda15b1441c44d05d6b790829cc30ce73b
balachandar06 <42191946+balachandar06@users.noreply.github.com>
2021-01-05 16:44:14 +0530

    Create codetest.java

---
+++ codetest.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+    String str1 = "agettkgaeee";
+      System.out.println ("The repeated string is: " + str1);
+    for (int i = 0; i < str1.length (); i++)
+      {
+	boolean unique = true;
+	for (int j = 0; j < str1.length (); j++)
+	  {
+	    if (i != j && str1.charAt (i) == str1.charAt (j))
+	      {
+		unique = false;
+		break;
+	      }
+	  }
+	if (unique)
+	  {
+	    System.out.println ("The non-repeated string is :" +
+				str1.charAt (i));
+	    break;
+	  }
+      }
+  }
+}
